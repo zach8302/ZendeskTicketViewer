88d48b53c2c45d1531b0efa6c0bbad6de57f344b
Zach Adler <zach8302@berkeley.edu>
2021-11-22 17:49:02 -0800

    Added preliminary search parameters

--- tickets.py
+++ tickets.py
@@ -1,16 +1,22 @@
-from zendesk_api import list_tickets, show_ticket, count_tickets
+from zendesk_api import list_tickets, show_ticket
 
 class Ticket():
-    def __init__(self, id, priority, subject, description):
+    def __init__(self, id, url, status, priority, subject, description, created_at):
         self.id = id
+        self.url = url
+        self.status = status
         self.priority = priority
         self.subject = subject
         self.description = description
+        self.created_at = created_at
     def __str__(self):
-        return f"[{self.id}] {self.subject}"
+        return f"[{self.id}] Created: {self.created_at} Subject: {self.subject}"
 
-def get_tickets():
-    tickets_raw = list_tickets()
+def get_tickets(params):
+    payload = {}
+    if "sort_by" in params:
+        payload["sort_by"] = params["sort_by"]
+    tickets_raw = list_tickets(payload)
     return parse_raw_list(tickets_raw['tickets'])
     
 def get_ticket(id):
@@ -24,7 +30,10 @@ def parse_raw_list(lst):
 
 def parse_raw(data):
     id = data['id']
+    url = data['url']
+    status = data['status']
     priority = data['priority']
     subject = data['subject']
     description = data['description']
-    return Ticket(id, priority, subject, description)
+    created_at = data["created_at"]
+    return Ticket(id, url, status, priority, subject, description, created_at)
--- user_interface.py
+++ user_interface.py
@@ -1,20 +1,25 @@
 from tickets import *
 
 def start():
-    commands = {"view":view_tickets, "select":view_ticket}
+    commands = {"view":view_tickets, "select":view_ticket, "params":set_params}
+    params = {}
+    print("Zendesk Ticket Viewer\nTo view all tickets enter: view\nTo view a specific ticket enter: select")
     while True:
         print("Enter a Command:")
         command = input()
         if command in commands:
-            commands[command]()
+            commands[command](params)
 
 
-def view_tickets():
-    tickets = get_tickets()
-    if len(tickets) > 25:
+
+def view_tickets(params):
+    tickets = get_tickets(params)
+    num = len(tickets)
+    if num > 25:
         i = 0
-        while i <= len(tickets):
-            for t in tickets[i:i + 25]:
+        while i < num:
+            print(f"Showing Tickets {i + 1} through {i + 25} of {num}")
+            for t in tickets[i:i+25]:
                 print(t)
             command = input()
             if command == "next":
@@ -25,8 +30,36 @@ def view_tickets():
         for t in tickets:
             print(t)
 
-def view_ticket():
+def view_ticket(params):
     id = input()
-    print(get_ticket(id))
+    ticket = get_ticket(id)
+    s = f"\n{ticket.url}\n[{ticket.id}]\nSubject: {ticket.subject}\nCreated: {ticket.created_at}\nStatus: {ticket.status}\nPriority: {ticket.priority}\nDescription: {ticket.description}"
+    print(s)
+
+def set_params(params):
+    # status = input()
+    # if status:
+    #     params["status"] = status
+    # while True:
+    #     before = input()
+    #     if not before:
+    #         break
+    #     params["before"] = before
+    # while True:
+    #     after = input()
+    #     if not after:
+    #         break
+    #     params["after"] = after
+    while True:
+        valid = {"created_at", "id", "subject"}
+        sort_by = input()
+        if sort_by in valid:
+            params["sort_by"] = sort_by
+            break
+    return params
+
+    
+
+
 
 start()
