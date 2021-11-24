0c2f1a7f8e50b8e0122d6f01c7cf2cc61924c0ee
Zach Adler <zach8302@berkeley.edu>
2021-11-24 09:53:36 -0800

    Added error messages and improved UI

--- tickets.py
+++ tickets.py
@@ -1,4 +1,5 @@
 from zendesk_api import list_tickets, show_ticket
+from collections import defaultdict
 
 class Ticket():
     def __init__(self, id, url, status, priority, subject, description, created_at):
@@ -9,33 +10,51 @@ class Ticket():
         self.subject = subject
         self.description = description
         self.created_at = created_at
+
     def __str__(self):
         return f"[{self.id}] Created: {self.created_at} Subject: {self.subject}"
+
+    def dateCompare(self, date):
+        num1 = int(self.created_at[0:4] + self.created_at[5:7] + self.created_at[8:10])
+        num2 = int(date[6:10]+ date[0:2] + date[3:5])
+        if num1 > num2:
+            return 1
+        elif num1 == num2:
+            return 0
+        else:
+            return -1
+
 
 def get_tickets(params):
     payload = {}
     if "sort_by" in params:
         payload["sort_by"] = params["sort_by"]
     tickets_raw = list_tickets(payload)
-    return parse_raw_list(tickets_raw['tickets'], params)
+    if tickets_raw == 401:
+        print("[401] Failed to authenticate. Please try again later")
+        exit(401)
+    tickets = parse_raw_list(tickets_raw['tickets'])
+    return filter_tickets(tickets, params["status"], params["before"], params["after"])
     
 def get_ticket(id):
-    return parse_raw(show_ticket(id)['ticket'])
+    ticket_raw = show_ticket(id)
+    if ticket_raw == 401:
+        print("[401] Failed to authenticate. Please try again later")
+        exit(401)
+    if ticket_raw == 404:
+        print("[404] Invalid Ticket ID. Please try again")
+        return
+    print(ticket_raw)
+    return parse_raw(ticket_raw['ticket'])
 
-def parse_raw_list(lst, params):
-    status = ""
-    if "status" in params:
-        status = params["status"]
+def parse_raw_list(lst):
     tickets = []
     for data in lst:
-        ticket = parse_raw(data, status)
-        if ticket:
-            tickets.append(ticket)
+        ticket = parse_raw(data)
+        tickets.append(ticket)
     return tickets
 
-def parse_raw(data, status=""):
-    if status and status != data['status']:
-        return None
+def parse_raw(data):
     id = data['id']
     url = data['url']
     status = data['status']
@@ -44,3 +63,10 @@ def parse_raw(data, status=""):
     description = data['description']
     created_at = data["created_at"]
     return Ticket(id, url, status, priority, subject, description, created_at)
+
+def filter_tickets(tickets, status, before, after):
+    filtered = []
+    for ticket in tickets:
+        if (not status or status == ticket.status) and (not before or ticket.dateCompare(before) == -1) and (not after or ticket.dateCompare(after) == 1):
+            filtered.append(ticket)
+    return filtered
--- user_interface.py
+++ user_interface.py
@@ -1,34 +1,41 @@
+import re
 from tickets import *
+from collections import defaultdict
 
 def start():
     commands = {"view":view_tickets, "select":view_ticket, "params":set_params}
-    params = {}
-    print("Zendesk Ticket Viewer\nTo view all tickets enter: view\nTo view a specific ticket enter: select")
+    params = defaultdict(lambda: None)
+    print("Zendesk Ticket Viewer\nTo view all tickets enter: view\nTo view a specific ticket enter: select\nTo set search parameters enter: params")
     while True:
         print("Enter a Command:")
         command = input()
         if command in commands:
             commands[command](params)
 
-
-
 def view_tickets(params):
     tickets = get_tickets(params)
     num = len(tickets)
-    if num > 25:
+    if num == 0:
+        print("No tickets found that match your criteria\n")
+    elif num > 25:
         i = 0
         while i < num:
+            print('\n')
             print(f"Showing Tickets {i + 1} through {i + 25} of {num}")
             for t in tickets[i:i+25]:
                 print(t)
+            print("Enter next to view the next page")
+            print("Enter exit to exit the ticket viewer")
             command = input()
             if command == "next":
                 i += 25
             elif command == "exit":
                 break
     else:
+        print('\n')
         for t in tickets:
             print(t)
+    params.clear()
 
 def view_ticket(params):
     id = input()
@@ -41,26 +48,34 @@ def set_params(params):
     status = input()
     if status:
          params["status"] = status
-    # while True:
-    #     before = input()
-    #     if not before:
-    #         break
-    #     params["before"] = before
-    # while True:
-    #     after = input()
-    #     if not after:
-    #         break
-    #     params["after"] = after
+    date = r"\d{2}/\d{2}/\d{4}"
     while True:
+        print("I want tickets from after (MM/DD/YYYY):")
+        after = input()
+        if not after:
+            break
+        if re.match(date, after):
+            params["after"] = after
+            break
+        print("Invalid format")
+    while True:
+        print("I want tickets from before (MM/DD/YYYY):")
+        before = input()
+        if not before:
+            break
+        if re.match(date, before):
+            params["before"] = before
+            break
+        print("Invalid format")
+    while True:
+        print("Sort by (id, created_by, subject):")
         valid = {"created_at", "id", "subject"}
         sort_by = input()
+        if not sort_by:
+            break
         if sort_by in valid:
             params["sort_by"] = sort_by
             break
     return params
-
-    
-
-
 
 start()
